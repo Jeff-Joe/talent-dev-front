cf8639fe25cc278259502bc80a51a4e0bd28d0bd
Jeff-Joe <91124403+Jeff-Joe@users.noreply.github.com>
2024-06-25 14:04:41 +1200

    Third Final

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import "semantic-ui-css/semantic.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //<React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
   //</React.StrictMode>
 );
 
